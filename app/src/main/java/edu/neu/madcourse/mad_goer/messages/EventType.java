71def4416b5c144159c47c5b30257b94031c9fa6
Sida Zhang <sidazhang.97@gmail.com>
2022-07-26 16:00:22 -0400

    adjust icons and categories

--- app/src/main/java/edu/neu/madcourse/mad_goer/messages/EventType.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/messages/EventType.java
@@ -1,11 +1,20 @@
 package edu.neu.madcourse.mad_goer.messages;
 
-public enum EventType {EDUCATION("Education"),Sports,Fitness,Technology,Travel,Outdoor,Games,
-    Art, Culture, Career, Business, Community, Dancing, Health, Hobbies, Movement,
-    Language, Music, Family, Pets, Religion, Science;
+public enum EventType {EDUCATION("Education"), SPORTS("Sports"),
+    FITNESS("Fitness"), TECHNOLOGY("Technology"),
+    TRAVEL("Travel"), OUTDOOR("Outdoor"), GAMES("Games"),
+    ART("Art"), CULTURE("Culture"), CAREER("Career"),
+    BUSINESS("Business"), COMMUNITY("Community"), DANCING("Dancing"),
+    HEALTH("Health"), HOBBIES("Hobbies"), MOVEMENT("Movement"),
+    LANGUAGE("Language"), MUSIC("Music"), FAMILY("Family"),
+    PETS("Pets"), RELIGION("Religion"), SCIENCE("Science");
 
     private String category;
     private EventType(String category){
         this.category = category;
     }
 }
+
+//public enum EventType {Education,Sports,Fitness,Technology,Travel,Outdoor,Games,
+//    Art, Culture, Career, Business, Community, Dancing, Health, Hobbies, Movement,
+//    Language, Music, Family, Pets, Religion, Science}
