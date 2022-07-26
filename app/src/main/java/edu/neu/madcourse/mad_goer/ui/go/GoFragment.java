3fadc5e0e3d5762441b154bffb098afc634830f7
YANG YANG <yang.yang16@northeastern.edu>
2022-07-26 17:35:17 -0400

    updated go fragment, home fragment

--- app/src/main/java/edu/neu/madcourse/mad_goer/ui/go/GoFragment.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/ui/go/GoFragment.java
@@ -7,18 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import edu.neu.madcourse.mad_goer.MainActivity;
 import edu.neu.madcourse.mad_goer.databinding.Fragment2GoBinding;
+import edu.neu.madcourse.mad_goer.messages.Event;
+import edu.neu.madcourse.mad_goer.ui.recycleview.EventAdapter;
 
 public class GoFragment extends Fragment {
 
     private Fragment2GoBinding binding;
     private String timePattern = "yyyy-MM-dd HH:mm:ss z";
     private DateFormat df = new SimpleDateFormat(timePattern);
+
+    private ArrayList<Event> eventList;
+    private RecyclerView recyclerView;
 
 
 
@@ -29,8 +37,20 @@ public class GoFragment extends Fragment {
         binding = Fragment2GoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        RecyclerView recyclerView = binding.rvGofrag;
 
         MainActivity activity = (MainActivity) getActivity();
+        //TODO: maybe do filter of event list here?
+        eventList = activity.getTotalEventList();
+
+        EventAdapter eventAdapter = new EventAdapter(eventList,getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(eventAdapter);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.scrollToPosition(eventList.size()-1);
 
         return root;
     }
