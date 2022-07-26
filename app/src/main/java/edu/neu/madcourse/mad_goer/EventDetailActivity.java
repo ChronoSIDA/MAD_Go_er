08e2027fd89c5edc8e5e439447ac5fe086320fd4
huangyiqian <huang.yiqia@northeastern.edu>
2022-07-26 16:15:39 -0400

    -Event Detail

--- app/src/main/java/edu/neu/madcourse/mad_goer/EventDetailActivity.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/EventDetailActivity.java
@@ -2,14 +2,18 @@ package edu.neu.madcourse.mad_goer;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.neu.madcourse.mad_goer.messages.Event;
+import edu.neu.madcourse.mad_goer.ui.PasswordDialog;
 
 
 public class EventDetailActivity extends AppCompatActivity {
     private Event event;
+    private Button joinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,18 @@ public class EventDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_detail);
 //        inputText = findViewById(R.id.city_et);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        joinBtn = (Button) findViewById(R.id.join_btn);
+        joinBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                passwordDialog();
+            }
+        });
 
+    }
+
+    public void passwordDialog(){
+        PasswordDialog passwordDialog = new PasswordDialog();
+        passwordDialog.show(getSupportFragmentManager(), "password dialog");
     }
 }
