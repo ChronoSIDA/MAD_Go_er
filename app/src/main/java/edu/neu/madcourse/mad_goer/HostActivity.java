c06a89b85a323d98442ae800c9e6816176a1cf59
huangyiqian <huang.yiqia@northeastern.edu>
2022-07-28 11:15:53 -0400

    -Interest & Host activity

---
+++ app/src/main/java/edu/neu/madcourse/mad_goer/HostActivity.java
@@ -0,0 +1,4 @@
+package edu.neu.madcourse.mad_goer;
+
+public class HostActivity {
+}
---
+++ app/src/main/java/edu/neu/madcourse/mad_goer/InterestActivity.java
@@ -0,0 +1,30 @@
+package edu.neu.madcourse.mad_goer;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.neu.madcourse.mad_goer.messages.User;
+
+
+public class InterestActivity extends AppCompatActivity {
+    private User user;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+
+
+        joinBtn = (Button) findViewById(R.id.btn_join_detail);
+        hostTV = (TextView) findViewById(R.id.txt_host_detail);
+
+
+
+    }
+}
