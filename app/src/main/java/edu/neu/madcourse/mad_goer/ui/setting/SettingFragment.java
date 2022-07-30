b393313b4f87005a8cecb0e58371d228764be1d9
XueTang422 <tang.1485@osu.edu>
2022-07-30 16:22:00 -0400

    settings page updated.

--- app/src/main/java/edu/neu/madcourse/mad_goer/ui/setting/SettingFragment.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/ui/setting/SettingFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,6 +15,11 @@ import edu.neu.madcourse.mad_goer.databinding.Fragment4SettingBinding;
 public class SettingFragment extends Fragment {
 
     private Fragment4SettingBinding binding;
+    private Button button_myHost, button_interest, button_accountSec,
+            button_emergencyCont, button_language, button_aboutGoer,
+            button_switchAccount, button_logOut;
+    private TextView txt_username, txt_privacy, txt_system;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -20,6 +27,23 @@ public class SettingFragment extends Fragment {
         binding = Fragment4SettingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        button_myHost = binding.buttonMyHostSetting;
+        button_myHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        button_interest = binding.buttonInterestSetting;
+        button_accountSec = binding.buttonAccountSecSetting;
+        button_emergencyCont = binding.buttonEmergencyContSetting;
+        button_language = binding.buttonLanguageSetting;
+        button_aboutGoer = binding.buttonAboutGoerSetting;
+        button_switchAccount = binding.buttonSwitchAccountSetting;
+        button_logOut = binding.buttonLogOutSetting;
+        txt_username = binding.txtUsernameSetting;
+        txt_privacy = binding.txtPrivacySetting;
+        txt_system = binding.txtSystemSetting;
         return root;
     }
 
