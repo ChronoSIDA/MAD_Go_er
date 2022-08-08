408a21eb9e79c60747ab516947178eb426bd50d0
Sida Zhang <sidazhang.97@gmail.com>
2022-08-08 18:43:13 -0400

    changed icon id

--- app/src/main/java/edu/neu/madcourse/mad_goer/ui/recycleview/EventAdapter.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/ui/recycleview/EventAdapter.java
@@ -50,7 +50,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.Viewholder>{
         int id = context.getResources().getIdentifier(getCategory, "drawable", context.getPackageName());
         holder.eventIcon.setImageResource(id);
         holder.eventName.setText(event.getEventName());
-        // 123
         holder.hostName.setText("Host: " + event.getHost().getUserID());
 
 
