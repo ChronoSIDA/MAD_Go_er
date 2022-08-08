75105d3c8908a57cfb9195ff145cf8e90a72f655
Sida Zhang <sidazhang.97@gmail.com>
2022-08-08 19:54:37 -0400

    comment and edit text layout

--- app/src/main/java/edu/neu/madcourse/mad_goer/ui/home/HomeFragment.java
+++ app/src/main/java/edu/neu/madcourse/mad_goer/ui/home/HomeFragment.java
@@ -22,7 +22,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 
